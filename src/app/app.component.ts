0623093c8e5cb4b02caed2e66f3925bb347c9bf3
Paul Buys <paul.buys1@gmail.com>
2018-08-03 22:54:21 +0200

    first deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Product } from '../assets/models/product';
+import { DataService } from '../app/services/data.service';
+
+interface TreatmentProtocol {
+  protocolId:number;
+  patientName:string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'treatment';
+  
+  constructor(private patientService: DataService){
+  }
+  ngOnInit(){
+  }
+
+  getTestData(){
+    this.patientService.getTestData();
+  }
+
+  uniqueID:number = 11111;
+  products: Product[] = [];
+
+  treatmentProtocols:TreatmentProtocol[] = [];
+
+  addItem(event){
+    this.products.push(event);
+  }
+  patientModel:any;
+  patientSearch:boolean = true;
+
+  addMedicationInfo(event){
+    this.patientModel = event;
+    this.patientSearch = false;
+  }
+
+  editProtocol(event){
+  }
+  
+  submitProtocol(){
+    this.patientSearch = true;
+
+    var value = {
+      protocolId:this.uniqueID,
+      patientName: this.patientModel.name
+    }
+    this.treatmentProtocols.push(value);
+    this.products = [];
+    this.uniqueID++;
+  }
 }
