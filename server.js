f038243d137083ca128fd610afd8fc55a19dca4a
Paul Buys <paul.buys1@gmail.com>
2018-08-06 08:59:52 +0200

    testing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('./dist/treatment'));
 
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname, '/dist/treatment/index.html'));
-});
+}); 
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log('Server started');
