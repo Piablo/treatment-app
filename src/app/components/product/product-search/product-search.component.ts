6431fa935cece7b0cb58d5b09fe6e1e581365552
Paul Buys <paul.buys1@gmail.com>
2018-08-08 16:04:03 +0200

    changes to product searchbar

--- src/app/components/product/product-search/product-search.component.ts
+++ src/app/components/product/product-search/product-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-product-search',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-search.component.css']
 })
 export class ProductSearchComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor(private dataService: DataService) { }
+  
   ngOnInit() {
   }
+  
+  products:any;
+  product: any;
+  filteredProducts: any[];
+  url = 'api/treatmentprotocolproducts/Search?searchOptions.';
+  
+  filterProductSingle(event, inputField) {
+    let query = event.query;
 
+    this.dataService.get(this.url, inputField, query).then(products => {
+      this.filteredProducts = this.filterProduct(query, products);
+    });
+  }
+  
+  filterProduct(query, products: any[]):any[] {
+    //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+    let filtered : any[] = [];
+    for(let i = 0; i < products.length; i++) {
+      let product = products[i];
+      if(product.Description.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(product);
+      }
+    }
+    return filtered;
+  }
+  
 }
